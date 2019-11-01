f474627690a48440405ea4aacd938d2a3b4a6f52
Homero Batista Kzam <homero.kzam@gmail.com>
2019-11-01 06:28:11 -0300

    Simplificação do código para reproduzir o problema

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'reactn';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Button, DataGrid, Popup, Template, Form } from 'devextreme-react';
+import { Item, SimpleItem } from 'devextreme-react/form';
+import { Column, Paging, Editing, LoadPanel } from 'devextreme-react/data-grid';
+import ArrayStore from 'devextreme/data/array_store';
+import CustomStore from 'devextreme/data/custom_store';
+
+const App = (props) => {
+
+  const storeVazia = new ArrayStore({ key: "oid", data: [] });
+
+  const currentData = { pagarValeTransporteEmFolha: true };
+
+  const [dataSourceFeriados, setDataSourceFeriados] = useState(storeVazia);
+
+  const newRecord = false;
+
+  const handleOnClickSalvar = (e) => { console.log(e); }
+  const handleOnClickCancelar = (e) => { console.log(e); }
+
+  const storeFeriados = new CustomStore({
+    key: "oid",
+    load: async () => {
+      const dados = [{ oid: 1, data: '10/09/2019', nome: 'Feriado1' }];
+      return (dados);
+    }
+  });
+  
+  useEffect(() => {
+
+    setDataSourceFeriados(storeFeriados);
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Popup visible={true} dragEnabled={false} closeOnOutsideClick={false} title={'Teste'} showCloseButton={false} showTitle={true} width={500} height={400}>
+        <Form formData={currentData} repaintChangesOnly={true}  colCount={1}>
+
+          <SimpleItem dataField={'pagarValeTransporteEmFolha'} editorType={'dxCheckBox'} label={ {text: '', visible: false}} editorOptions={{ disabled: false, text: 'Pagar VT Em Folha' }} visible={newRecord} />
+
+        </Form>
+
+        &nbsp;
+
+        <DataGrid dataSource={dataSourceFeriados} allowColumnReordering={true} showBorders={true} >
+
+          <Paging defaultPageSize={5} />
+          <Editing mode={'row'} useIcons={true} allowUpdating={true} allowDeleting={true} />
+          <LoadPanel enabled={true} />
+
+          <Column type={'buttons'} width={100} buttons={['edit', 'delete']} />
+          <Column dataField={'oid'} visible={false} />
+          <Column dataField={'data'} caption={'Data'} dataType={'date'} format={'dd/MM/yyyy'} sortIndex={0} sortOrder={'asc'} width={'120px'} />
+          <Column dataField={'nome'} caption={'Nome'} />
+        </DataGrid>
+
+      </Popup>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.compact.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
